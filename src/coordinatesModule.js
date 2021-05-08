3e2949f08903007ce865e016f773608d675965ff
Cynto <lucagaravello@outlook.com>
2021-05-08 19:09:00 +0200

    moved from http to https in coordinates module

--- src/coordinatesModule.js
+++ src/coordinatesModule.js
@@ -12,10 +12,9 @@ export default async function getCoordinates(location) {
     return coordArray;
   } catch {
     const response = await fetch(
-      `http://api.positionstack.com/v1/forward?access_key=e1ac105a4456be2d9e8b62718e4f0bb3&query=${location}`,
+      `https://api.positionstack.com/v1/forward?access_key=e1ac105a4456be2d9e8b62718e4f0bb3&query=${location}`,
     );
     const coordinates = await response.json();
-    console.log(coordinates.results[0].geometry)
     const latCord = coordinates.data[0].latitude;
     const longCord = coordinates.data[0].longitude;
     const coordArray = [latCord, longCord];
